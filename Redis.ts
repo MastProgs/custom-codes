863f0dd2b4e7097323764d24e983220168b4dd8a
김형준 <khjkhj2804@naver.com>
2021-12-06 19:37:32 +0900

    Add files via upload

---
+++ Redis.ts
@@ -0,0 +1,71 @@
+const redis = require('redis');
+const { promisify } = require("util");
+
+export class Redis {
+    public RedisClient: any;
+    public SelectAsync: any;
+    public HMGetAsync: any;
+    public HMSetAsync: any;
+    public ZAddAsync: any;
+    public ZRangeAsync: any;
+    public ZRangeByScoreAsync: any
+    public ZRevRangeAsync: any;
+    public ZRevRankAsync: any;
+    public ZRemAsync: any;
+    public SAddAsync: any;
+    public SRemAsync: any
+    public SMembers: any
+    public ZScoreAsync: any;
+    public ZInterStoreAsync: any;
+    public KeysAsync: any;
+    public ZCardAsync: any;
+    public ZScanAsync: any;
+    public ZUnionstoreAsync: any;
+
+    private static redisMap: Map<number, Redis> = new Map<number, Redis>()
+
+    public static Inst(index: number) {
+        return this.redisMap.get(index)
+    }
+
+    private constructor() {
+        this.Init();
+    }
+
+    private async Init() {
+        //console.log('Begin RedisManager Init()');
+        this.RedisClient = redis.createClient(6379, "127.0.0.1");
+        this.RedisClient.on("error", function (error) {
+            console.error(error);
+        });
+
+        this.SelectAsync = promisify(this.RedisClient.select).bind(this.RedisClient)
+        this.HMGetAsync = promisify(this.RedisClient.hmget).bind(this.RedisClient);
+        this.HMSetAsync = promisify(this.RedisClient.hmset).bind(this.RedisClient);
+        this.SAddAsync = promisify(this.RedisClient.sadd).bind(this.RedisClient);
+        this.SRemAsync = promisify(this.RedisClient.srem).bind(this.RedisClient)
+        this.SMembers = promisify(this.RedisClient.smembers).bind(this.RedisClient)
+        this.ZAddAsync = promisify(this.RedisClient.zadd).bind(this.RedisClient);
+        this.ZRangeAsync = promisify(this.RedisClient.zrange).bind(this.RedisClient);
+        this.ZRangeByScoreAsync = promisify(this.RedisClient.zrangebyscore).bind(this.RedisClient)
+        this.ZRevRangeAsync = promisify(this.RedisClient.zrevrange).bind(this.RedisClient);
+        this.ZRevRankAsync = promisify(this.RedisClient.zrevrank).bind(this.RedisClient);
+        this.ZRemAsync = promisify(this.RedisClient.zrem).bind(this.RedisClient);
+        this.ZScoreAsync = promisify(this.RedisClient.zscore).bind(this.RedisClient);
+        this.ZInterStoreAsync = promisify(this.RedisClient.zinterstore).bind(this.RedisClient);
+        this.KeysAsync = promisify(this.RedisClient.keys).bind(this.RedisClient);
+        this.ZCardAsync = promisify(this.RedisClient.zcard).bind(this.RedisClient);
+        this.ZScanAsync = promisify(this.RedisClient.zscan).bind(this.RedisClient);
+        this.ZUnionstoreAsync = promisify(this.RedisClient.zunionstore).bind(this.RedisClient);
+
+        //console.log('End RedisManager Init()');
+    }
+
+    public static async MakeAllConnection() {
+        for (let index = 0; index < 16; index++) {
+            let red = new this()
+            this.redisMap.set(index, red)
+            await red.SelectAsync(index)
+        }
+    }
+}
